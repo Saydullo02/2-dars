6a408d4b9e8451970621f4d2f552d922d14e1c88
Saydullo02 <saydulloatayev777@gmail.com>
2023-03-29 20:54:24 +0500

    The end

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+// Hmtl document
 
+// var minutus = 230;
+// var newHours = Math.floor(minutus / 60);
+// var newMinuts = minutus  %  60
+// // console.log(Math.floor(Math.random() * 20));
+// console.log(`${newHours} soat ${newMinuts} minut`);
 
+// Uss and Euro prizes
+var USS_UZB = 9433.34;
+var EUR_UZB = 10354.03;
 
-var minutus = 230;
-var newHours = Math.floor(minutus / 60);
-var newMinuts = minutus % 60
-console.log(Math.floor(Math.random() * 20));
-console.log(`${newHours} soat ${newMinuts} minut`);
+// Harajatlar
+var ticketPrize = 500;
+var forHotel = 250;
+var forMuseum = 120;
+var resultForTicketandhotel = ticketPrize + forHotel;
+
+var totalPrize = Math.round((USS_UZB * resultForTicketandhotel) + (EUR_UZB * forMuseum ));
+var totalPrompt = prompt("Umumiy harajatlar so'mmasi" , 8317489 );
+
+if (totalPrize <= totalPrompt) {
+    console.log("Oq yo'l, Alisher!");
+}else{
+    console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+}
+
