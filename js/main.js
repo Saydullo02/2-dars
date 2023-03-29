f14ef9463ad7e8962ee07e19deb410e8733cb309
Saydullo02 <saydulloatayev777@gmail.com>
2023-03-29 18:52:12 +0500

    second page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+
+var minutus = 230;
+var newHours = Math.floor(minutus / 60);
+var newMinuts = minutus % 60
+console.log(Math.floor(Math.random() * 20));
+console.log(`${newHours} soat ${newMinuts} minut`);
